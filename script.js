c2acb05aadcc0b580db674e0e8e09a022eb35964
Vivek Budithi <vivek.budithi@gmail.com>
2020-11-06 04:16:08 +0930

    Card layout

--- script.js
+++ script.js
@@ -3,22 +3,51 @@ const APIURL =
 
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280/'
 
+const main = document.querySelector('main');
+
 async function getMovies() {
     const resp = await fetch(APIURL);
     const respData = await resp.json();
     
     console.log(respData);
      
-    respData.results.forEach(movie => {
-        const img = document.createElement('img');
-        img.src = IMGPATH + movie.poster_path;
+    respData.results.forEach((movie) => {
+       
+        const { poster_path, title, vote_average } = movie;
 
-        document.body.appendChild(img);
-    })
+       const movieEl = document.createElement("div");
+       movieEl.classList.add("movie");
+
+       movieEl.innerHTML = `
+                <img
+                    src ="${IMGPATH + poster_path}" 
+                    alt="${title}"
+                />         
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <i class="fas fa-star-half-alt" >
+                    <hr />
+                    <span class = "${getClassByrate(vote_average)}">${vote_average}</span>
+                    </i>
+                   
+                </div>
+            `;
+
+            main.appendChild(movieEl );
+        });
 
     return respData;
 }
 
+function getClassByrate(vote) {
+    if(vote >= 8) {
+         return 'green';
+    } else if (vote >=5){
+        return 'orange';
+    }else{
+         return 'red';
+    }
+}
 getMovies();
 
 
